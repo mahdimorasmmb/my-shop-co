34a28168f1e0054ba56ad97575c38ae6787d9665
mahdi morasaeiyan <m.morasaeiyan@irantalent.com>
2023-01-07 21:57:30 +0330

    add new data and change

--- src/widgets/CheckoutProduct.tsx
+++ src/widgets/CheckoutProduct.tsx
@@ -1,11 +1,11 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import Image from "next/legacy/image";
 import React from "react";
-import { urlFor } from "../../../sanity";
-import Currency from "react-currency-formatter";
-import { useDispatch } from "react-redux";
-import { removeFromBasket } from "../../redux/basketSlice";
-import { toast } from "react-toastify";
+// import { urlFor } from "../../../sanity";
+// import Currency from "react-currency-formatter";
+// import { useDispatch } from "react-redux";
+// import { removeFromBasket } from "../../redux/basketSlice";
+// import { toast } from "react-toastify";
 
 interface Props {
   items: Product[];
@@ -13,29 +13,29 @@ interface Props {
 }
 
 export const CheckoutProduct = ({ id, items }: Props) => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const removeItemFromBasket = () => {
-    dispatch(removeFromBasket({ id }));
+  // const removeItemFromBasket = () => {
+  //   dispatch(removeFromBasket({ id }));
 
-    toast.error(`${items[0]?.title} removed from basket`, {
-      position: "bottom-center",
-    });
-  };
+  //   toast.error(`${items[0]?.title} removed from basket`, {
+  //     position: "bottom-center",
+  //   });
+  // };
   return (
     <div className="flex flex-col gap-x-4 border-b border-gray-300 pb-5 lg:flex-row">
       <div className="relative h-44 w-44">
-        <Image
+        {/* <Image
           src={urlFor(items[0]?.image[0]).url()}
           layout="fill"
           objectFit="contain"
           alt={items[0]?.title}
-        />
+        /> */}
       </div>
       <div className="flex flex-1 items-end lg:items-center">
         <div className="flex-1 space-y-4 ">
           <div className="flex flex-col gap-x-8 text-xl lg:flex-row lg:text-2xl">
-            <h4 className="font-semibold lg:w-96 ">{items[0]?.title}</h4>
+            {/* <h4 className="font-semibold lg:w-96 ">{items[0]?.title}</h4> */}
             <p className="flex items-end gap-x-1 font-semibold ">
               {items.length}
               <ChevronDownIcon className="h-6 w-6 text-blue-500" />
@@ -49,17 +49,17 @@ export const CheckoutProduct = ({ id, items }: Props) => {
         </div>
         <div className="flex flex-col items-end space-y-4 ">
           <h4 className="text-xl font-semibold lg:text-2xl">
-            <Currency
+            {/* <Currency
               quantity={items.reduce((total, item) => total + item.price, 0)}
               currency="USD"
-            />
+            /> */}
           </h4>
-          <button
+          {/* <button
             onClick={removeItemFromBasket}
             className="text-blue-500 hover:underline"
           >
             Remove
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
--- src/widgets/Product.tsx
+++ src/widgets/Product.tsx
@@ -1,36 +1,36 @@
 import { ShoppingCartIcon } from "@heroicons/react/outline";
 import Image from "next/legacy/image";
 import React from "react";
-import { urlFor } from "../../../sanity";
-import { useDispatch } from "react-redux";
-import { addToBasket } from "../../redux/basketSlice";
-import { toast } from "react-toastify";
+// import { urlFor } from "../../../sanity";
+// import { useDispatch } from "react-redux";
+// import { addToBasket } from "../../redux/basketSlice";
+// import { toast } from "react-toastify";
 
 interface Props {
   product: Product;
 }
 
 const Product = ({ product }: Props) => {
-  const dispatch = useDispatch();
-  const addItemToBasket = () => {
-    dispatch(addToBasket(product));
+  // const dispatch = useDispatch();
+  // const addItemToBasket = () => {
+  //   dispatch(addToBasket(product));
 
-    toast.success(`${product.title} add to Basket`, {
-      position: "bottom-center",
-    });
-  };
+  //   toast.success(`${product.title} add to Basket`, {
+  //     position: "bottom-center",
+  //   });
+  // };
   return (
     <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383c] p-8 md:h-[500px] md:w-[400px] md:p-10   ">
       <div className="relative h-64 w-full md:h-72">
-        <Image
+        {/* <Image
           src={urlFor(product.image[0]).url()}
           layout="fill"
           alt={product.title}
           objectFit="contain"
-        />
+        /> */}
       </div>
       <div className="flex flex-1 items-center justify-between space-x-3">
-        <div className="space-y-2 text-xl text-white md:text-2xl">
+        {/* <div className="space-y-2 text-xl text-white md:text-2xl">
           <p>{product.title}</p>
           <p>{product.price}</p>
         </div>
@@ -39,7 +39,7 @@ const Product = ({ product }: Props) => {
           onClick={addItemToBasket}
         >
           <ShoppingCartIcon className="h-8 w-8 text-white" />
-        </div>
+        </div> */}
       </div>
     </div>
   );
